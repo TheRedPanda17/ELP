0fa5f96fd536e135a61d73bdce233fd1808493d1
Caleb Robinson <caleb.robinson@life.church>
2020-09-28 14:43:17 -0500

    Added node modules, nav menu and homepage
    * Added node_modules for routing
    * Added nav menu
    * Created home.js

--- JS/react-travel-website/src/App.js
+++ JS/react-travel-website/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/home';
 
 function App() {
   return (
-    <div className="App">
-      Yo    
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home}/>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
